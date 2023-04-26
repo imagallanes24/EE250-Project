f15a849963a5e076025beb4c0dd8733663e833b3
imagallanes24 <89544214+imagallanes24@users.noreply.github.com>
2023-04-26 12:10:57 -0700

    dt temp change

--- main.py
+++ main.py
@@ -101,7 +101,7 @@ while True:
 
     dial = grovepi.analogRead(rotary_angle_sensor_port)
 
-    temp_range = int(dial / 1023 * 50) + 40
+    temp_range = int(dial / 1023) * 50 + 40
 
     if temperature > temp_range:
         grovepi.digitalWrite(relay_port, 1)
