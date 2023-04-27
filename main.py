b26dffc046632dbfa01185a13120a2e27d1ebf5e
imagallanes24 <89544214+imagallanes24@users.noreply.github.com>
2023-04-26 18:14:03 -0700

    emergency alert added

--- main.py
+++ main.py
@@ -118,8 +118,10 @@ if __name__ == '__main__':
             grovepi.digitalWrite(relay_port, 0)
             HVAC_on = False
 
+        emer_str = "EMERGENCY"
         if humidity > 80:
             grovepi.digitalWrite(buzzer_port, 1)
+            client.publish("imagalla/emergency", "{}".format(emer_str))
         else:
             grovepi.digitalWrite(buzzer_port, 0)
         
--- sub.py
+++ sub.py
@@ -6,11 +6,13 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("imagalla/temp")
     client.subscribe("imagalla/humid")
     client.subscribe("imagalla/HVAC")
+    client.subscribe("imagalla/emergencyalert")
 
     client.message_callback_add("imagalla/datetime", on_message_datetime)
     client.message_callback_add("imagalla/temp", on_message_from_temp)
     client.message_callback_add("imagalla/humid", on_message_from_humid)
     client.message_callback_add("imagalla/HVAC", on_message_from_HVAC)
+    client.message_callback_add("imagalla/emergencyalert", on_message_emergency)
 
 def on_message(client, userdata, msg):
     print(msg.topic + ": " + str(msg.payload, "utf-8"))
@@ -23,6 +25,8 @@ def on_message_from_humid(client, userdata, message):
     print("Current Humidity: " + message.payload.decode())
 def on_message_from_HVAC(client, userdata, message):
     print("AC: " + message.payload.decode() + "\n")
+def on_message_emergency(client, userdata, message):
+    print("" + message.payload.decode())
     
 if __name__ == '__main__':
     client = mqtt.Client()
